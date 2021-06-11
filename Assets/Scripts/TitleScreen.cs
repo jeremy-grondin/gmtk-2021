6fcd87baa4fdc57f4aa0aa749e4598986e104180
TODOROV Anatole <todorov.anatole@gmail.com>
2021-06-11 19:41:04 +0200

    add text mesh pro

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    Animator anim = null;
+    int levelToLoad = 0;
+
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void AnimLoadLevelWithIndex(int index)
+    {
+        levelToLoad = index;
+        anim.SetTrigger("LoadingLevel");
+    }
+
+    void LoadLevelWithIndex()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
