9c448ba6fc5b94deb16ef6464abfa311dc71654e
jeremy-grondin <jerem97480@gmail.com>
2021-06-11 21:54:06 +0200

    Player fire soul in mouse direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,9 +24,20 @@ public class Player : MonoBehaviour
 
         if (Input.GetMouseButtonDown(1))
         {
-            GameObject tempSoul = Instantiate(soul, transform.position, Quaternion.identity);
-            tempSoul.GetComponent<Rigidbody>().AddForce(new Vector3(10, 0, 0));
+            Vector3 pos = transform.position;
+            pos.z += 2.0f;
+            GameObject tempSoul = Instantiate(soul, pos, Quaternion.identity);
+            Vector3 dir = Vector3.zero;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Plane plane = new Plane(Vector3.up, 0.0f);
 
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                dir = new Vector3(hit.point.x, 0, hit.point.z) - new Vector3(transform.position.x, 0, transform.position.z);
+            }
+            dir.Normalize();
+            tempSoul.GetComponent<Rigidbody>().AddForce(dir * 100);
         }
     }
 }
