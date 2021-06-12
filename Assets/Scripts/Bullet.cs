6f9753451786cf90a1744641df7246f5a03f50ca
TODOROV Anatole <todorov.anatole@gmail.com>
2021-06-12 20:51:57 +0200

    remove destruction of bullet once it reach destination

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,18 +5,14 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
 
-    public Vector3 destination = Vector3.zero;
+    public Vector3 direction = Vector3.zero;
     [SerializeField] float speed = 0;
     [SerializeField] int damage = 0;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = destination - transform.position;
-        transform.position += dir.normalized * (speed * Time.deltaTime);
-
-        if (dir.magnitude < 0.1)
-            Destroy(gameObject);
+        transform.position += direction.normalized * (speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
