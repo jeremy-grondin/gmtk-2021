280bf8e52963e7e2d81c4283ec00f9a0484a15ea
Yoan <ydelalonde@gmail.com>
2021-06-13 01:55:24 +0200

    Added Routine on destroy for AI1

---
+++ Assets/Scripts/ActiveOtherMethode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveOtherMethode : MonoBehaviour
+{
+
+    [SerializeField] EnemyMove reference = null ;
+
+
+   public void DestroyRoutine()
+   {
+       reference.DestroyRoutine();
+   }
+}
